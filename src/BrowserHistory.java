1353b5056a9411de274456de3549276dee9e4af5
Mihail Hristov <mis6us@icloud.com>
2020-09-16 21:54:33 +0300

    Add final solution of the problem

--- src/BrowserHistory.java
+++ src/BrowserHistory.java
@@ -1,9 +1,34 @@
+import java.util.ArrayDeque;
 import java.util.Scanner;
 
 public class BrowserHistory {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String inputtedCommand = scanner.nextLine();
+
+        ArrayDeque<String> history = new ArrayDeque<>();
+
+        String currentPage = "";
+
+        while (!"Home".equals(inputtedCommand)){
+            if ("back".equals(inputtedCommand)){
+                if (history.size() <= 1) {
+                    System.out.println("no previous URLs");
+                    inputtedCommand = scanner.nextLine();
+                    continue;
+                }else {
+                    currentPage = history.pop();
+                }
+            }else {
+                history.push(currentPage);
+                currentPage = inputtedCommand;
+            }
+
+            System.out.println(currentPage);
+
+            inputtedCommand = scanner.nextLine();
+        }
 
     }
 }
